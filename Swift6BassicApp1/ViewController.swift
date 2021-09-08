a1da73039a86b90bb239e6af63c11f78f159ff27
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-09-08 17:07:27 +0900

    Initial Commit

---
+++ Swift6BassicApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6BassicApp1
+//
+//  Created by 西村拓晃 on 2021/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
